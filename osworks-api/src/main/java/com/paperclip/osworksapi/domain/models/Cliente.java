72b5dc0349a4f95522284d30332fcc8db17b145b
henrique-tavares <ricktavares76@gmail.com>
2020-06-18 18:40:34 -0300

    creating a cliente model

---
+++ osworks-api/src/main/java/com/paperclip/osworksapi/domain/models/Cliente.java
@@ -0,0 +1,41 @@
+package com.paperclip.osworksapi.domain.models;
+
+public class Cliente {
+    public Long id;
+    public String nome;
+    public String email;
+    public String telefone;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+}
