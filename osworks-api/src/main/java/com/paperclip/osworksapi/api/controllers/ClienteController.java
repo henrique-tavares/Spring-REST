2825441403f1039b499381b925c4b9f7e9d182f2
henrique-tavares <ricktavares76@gmail.com>
2020-06-19 16:03:32 -0300

    adicionando metodos  ao cliente controller

--- osworks-api/src/main/java/com/paperclip/osworksapi/api/controllers/ClienteController.java
+++ osworks-api/src/main/java/com/paperclip/osworksapi/api/controllers/ClienteController.java
@@ -1,30 +1,81 @@
 package com.paperclip.osworksapi.api.controllers;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
 
 import com.paperclip.osworksapi.domain.models.Cliente;
+import com.paperclip.osworksapi.domain.repositories.ClienteRepository;
+import com.paperclip.osworksapi.domain.services.CadastroClienteService;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/clientes")
 public class ClienteController {
 
-    @GetMapping("/clientes")
-    public List<Cliente> clientes() {
-        var cliente1 = new Cliente();
-        cliente1.setId(1L);
-        cliente1.setNome("João");
-        cliente1.setEmail("joao@mail.com");
-        cliente1.setTelefone("55 98888-1111");
+    @Autowired
+    private ClienteRepository clienteRepository;
 
-        var cliente2 = new Cliente();
-        cliente2.setId(2L);
-        cliente2.setNome("Maria");
-        cliente2.setEmail("maria@mail.com");
-        cliente2.setTelefone("55 97777-2222");
+    @Autowired
+    private CadastroClienteService cadastroClienteService;
 
-        return Arrays.asList(cliente1, cliente2);
+    @GetMapping
+    public List<Cliente> listar() {
+        return clienteRepository.findAll();
+    }
+
+    @GetMapping("/{clienteId}")
+    public ResponseEntity<Cliente> buscar(@PathVariable Long clienteId) {
+        Optional<Cliente> cliente = clienteRepository.findById(clienteId);
+
+        if (cliente.isPresent()) {
+            return ResponseEntity.ok(cliente.get());
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente adicionar(@Valid @RequestBody Cliente cliente) {
+        return cadastroClienteService.salvar(cliente);
+    }
+
+    @PutMapping("/{clienteId}")
+    public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId, @RequestBody Cliente cliente) {
+
+        if (!clienteRepository.existsById(clienteId)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        cliente.setId(clienteId);
+        cliente = cadastroClienteService.salvar(cliente);
+
+        return ResponseEntity.ok(cliente);
+    }
+
+    @DeleteMapping("/{clienteId}")
+    public ResponseEntity<Void> deletar(@PathVariable Long clienteId) {
+
+        if (!clienteRepository.existsById(clienteId)) {
+            return ResponseEntity.notFound().build();
+        }
+
+        cadastroClienteService.excluir(clienteId);
+
+        return ResponseEntity.noContent().build();
     }
 }
