37025ba73af88ed420a242d78f0af0321b2cbe83
henrique-tavares <ricktavares76@gmail.com>
2020-06-19 16:04:18 -0300

    adicionando validação

--- osworks-api/src/main/java/com/paperclip/osworksapi/domain/models/Cliente.java
+++ osworks-api/src/main/java/com/paperclip/osworksapi/domain/models/Cliente.java
@@ -1,9 +1,33 @@
 package com.paperclip.osworksapi.domain.models;
 
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
+
+    @NotBlank
+    @Size(max = 60)
     public String nome;
+
+    @NotBlank
+    @Email
+    @Size(max = 60)
     public String email;
+
+    @NotBlank
+    @Size(max = 20)
     public String telefone;
 
     public Long getId() {
@@ -38,4 +62,21 @@ public class Cliente {
         this.telefone = telefone;
     }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (!(obj instanceof Cliente)) {
+            return false;
+        }
+        Cliente other = (Cliente) obj;
+        return Objects.equals(id, other.id);
+    }
+    
 }
